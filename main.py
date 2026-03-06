656545d43f3941d40160fae19e8a82ff38e8cd5d
ferarau <fernanda.s.araujo9@aluno.senai.br>
2026-03-06 10:38:27 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,5 +6,23 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello World!!!</h1>"
 
+
+@app.route("/curso")
+def fer():
+    return "<h1>Desenvolvimento de sistemas, Fernanda</h1>"
+
+
+@app.route("/cidade")
+def pira():
+    
+    dados = {
+        "nome": "Piracicaba",
+        "estado": "São Paulo",
+        "população": 423000,
+        "fundação": "1767"
+    }
+
+    return dados
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
